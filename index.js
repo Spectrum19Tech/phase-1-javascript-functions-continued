898789c622be0a3e97c4c36e64299ea749aae265
Jonathan Bryant <spectrum19tech@gmail.com>
2022-01-11 07:43:16 -0800

    commit tree b46081c39434abf2fc7ab4536cbe427efd14ab3e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(stringParam = '*'){
+    return function(param1 = 'special') {
+        return `You are ${stringParam}${param1}${stringParam}!`
+    }
+}
